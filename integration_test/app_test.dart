ad06af4da6563dde0ebea151b0000bb104581745
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-09-16 20:34:17 -0300

    Migra para o Gradle Kotlin DSL e adiciona testes de integração
    Este commit migra os arquivos de configuração do Gradle de Groovy para Kotlin DSL (`.kts`).
    Além disso, adiciona um teste de integração básico (`integration_test/app_test.dart`) para verificar a funcionalidade do menu.
    
    Outras mudanças incluem:
    - Atualização da versão do Gradle Wrapper para 8.12.
    - Aumento da memória JVM para o Gradle em `android/gradle.properties`.
    - Adição da dependência `integration_test` em `pubspec.yaml`.
    - Inclusão de `.cxx/` no `.gitignore` do Android.

---
+++ integration_test/app_test.dart
@@ -0,0 +1,24 @@
+import 'package:client_control/main.dart' as app;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Integration Test', (tester) async {
+    app.main();
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('Menu'), findsNothing);
+    await Future.delayed(Duration(seconds: 2));
+
+    await tester.tap(find.byIcon(Icons.menu));
+    await Future.delayed(Duration(seconds: 2));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Menu'), findsOneWidget);
+    await Future.delayed(Duration(seconds: 2));
+  });
+}
